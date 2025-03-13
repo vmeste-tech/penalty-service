75eb52304679cfecd5928f865245d203aa7075d8
kolpakovee <eekolpakov@edu.hse.ru>
2025-03-13 18:43:19 +0300

    init spring project

---
+++ src/main/java/ru/kolpakovee/penalty_service/PenaltyServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.kolpakovee.penalty_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenaltyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenaltyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kolpakovee/penalty_service/PenaltyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kolpakovee.penalty_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenaltyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
